792ced088d06ce94df73ab637899c01d2f2e790a
Oscar Vega <os_ar1@hotmail.com>
2024-11-12 10:22:42 -0600

    Added modular JavaScript functions with import/export in 24.js and functions.js; updated HTML to load 24.js as a module

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+// functions.js - Module with basic arithmetic functions
+
+// Function to add two numbers
+export function sum(numb01, numb02) {
+  return numb01 + numb02;
+}
+
+// Function to subtract the second number from the first
+export function subtract(numb01, numb02) {
+  return numb01 - numb02;
+}
