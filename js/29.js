8cdcf663eff17656c7e65015d6e9abe37f53306a
Oscar Vega <os_ar1@hotmail.com>
2024-11-14 10:43:08 -0600

    Add dynamic updates to DOM elements using JavaScript
    - Modified text content of the heading element.
    - Set a new value for the 'name' input field.
    - Updated link text in '.navegation' to display sequential numbers starting from 1.
    - Enhanced forEach loop to start numbering at 1 for improved readability.

---
+++ js/29.js
@@ -0,0 +1,21 @@
+// Handling elements with JavaScript
+
+// Using document.querySelector() to select DOM elements
+// Select the first element with the class 'heading'
+const heading = document.querySelector(".heading");
+
+// querySelectorAll selects all matching elements with the specified selector
+const links2 = document.querySelectorAll(".navegation a"); // Selects all <a> elements within '.navegation'
+
+// Update the text content of the 'heading' element
+heading.textContent = 'Un nuevo heading...';
+
+// Selecting an input field and setting a new value
+const inputName = document.querySelector('#name');
+inputName.value = 'Un nuevo valor';
+
+// Log the NodeList of links to the console
+console.log(links2);
+
+// Update the text content of each link element in 'links2', starting with 'Nuevo enlace 1' up to 'Nuevo enlace 4'
+links2.forEach((navegation, i) => navegation.textContent = `Nuevo enlace ${i + 1}`);
